8daa3f1e867ea1b105b3ee4fc971fa0981c5d5c4
Guilherme Nobrega <gbn6699@gmail.com>
2024-02-01 14:00:10 -0300

    curso-laravel

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,18 @@ use App\Http\Controllers\Resource;
 |
 */
 
+Route::resource('produtos', ProdutoController::class);
+
 Route::get('/', [SiteController::class,'index'])->name('site.index');
 Route::get('/produto/{slug}', [SiteController::class, 'details'])->name('site.details');
 Route::get('/categoria/{id}', [SiteController::class,'categoria'])->name('site.categoria');
 
 Route::get('/carrinho', [CarrinhoController::class, 'carrinhoLista'])->name('site.carrinho');
+Route::post('/carrinho', [CarrinhoController::class,'adicionaCarrinho'])->name('site.addcarrinho');
 
+
+
+//Testes
 Route::get('/empresa', function(){
     return view('empresa');
 });
